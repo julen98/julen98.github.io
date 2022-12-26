c7cfda29327fb138759bb448a4a0f734a4246fe0
Julen <julenramon792@gmail.com>
2022-12-26 07:53:22 +0100

    v2

--- tritan/flutter_service_worker.js
+++ tritan/flutter_service_worker.js
@@ -55,8 +55,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ab0d1912db44f783dfd1cdde677f2a55",
-"/": "ab0d1912db44f783dfd1cdde677f2a55",
+"index.html": "904ebde58aca2fe16d8850f54e280cb8",
+"/": "904ebde58aca2fe16d8850f54e280cb8",
 "main.dart.js": "4139be9fb4f0619b5285218e32bdc0c1",
 "manifest.json": "446a687dcefd8253e95cdd33de4d2108",
 "version.json": "31122389fa2e05671a054f6806696dba"
